133a14fdb0c2373298a8ca47af482f5611c7a6df
Peggy <info@klemtech.nl>
2021-11-30 17:04:33 +0100

    First Assignment second delivery

--- index.js
+++
@@ -1,9 +0,0 @@
-console.log("Hello Winc Academy");
-        let firstName = 'Peggy';//name
-        console.log(firstName);
-
-        let age = '44'; //number string
-        console.log(age);
-
-        let newAge = 45;//number number
-        console.log(newAge);
---
+++ script.js
@@ -0,0 +1,36 @@
+console.log("Hello Winc Academy");
+        let firstName = 'Peggy';//name
+        console.log(firstName);
+
+        let oldAge = '44'; //number string
+        console.log(oldAge);
+
+        let newAge = 45;//number number
+        console.log(newAge);
+
+        // If is not possible, this is a reserved keyword
+        // Tried all in dev tools, now with answers
+
+        let sum = 4 + 4;
+        console.log("Totaal:", sum);
+
+        let sumStrings = "4" + "4"
+        console.log("Totaal met Strings:", sumStrings);
+
+        let subtraction = 10 - 3;
+        console.log("Vermindering:", subtraction);
+
+        let multiplication = 3 * 3;
+        console.log("Keer:", multiplication);
+
+        let division = 25 / 5;
+        console.log("DelenDoor:", division);
+
+        let modulus = 12 % 9;
+        console.log("Weet ik niet precies:", modulus);
+
+
+
+
+
+
