ecab37a5e4fae4061e53cc8575eb9ead52cf5ceb
Peggy <info@klemtech.nl>
2021-11-30 16:33:32 +0100

    Exercise one

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("Hello Winc Academy");
+        let firstName = 'Peggy';//name
+        console.log(firstName);
+
+        let age = '44'; //number string
+        console.log(age);
+
+        let newAge = 45;//number number
+        console.log(newAge);
